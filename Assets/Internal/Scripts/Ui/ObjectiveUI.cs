cfe1202d80f51235e36e60ab8996c91accd3ed35
shoshanimayan <shoshanimayan@gmail.com>
2023-11-20 18:17:47 -0800

    objectives menu

---
+++ Assets/Internal/Scripts/Ui/ObjectiveUI.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+
+namespace Ui
+{
+	public class ObjectiveUI: MonoBehaviour
+	{
+
+		///  INSPECTOR VARIABLES       ///
+		[SerializeField] TextMeshProUGUI _name;
+        [SerializeField] TextMeshProUGUI _description;
+        [SerializeField] Image _checkSpot;
+        [SerializeField] Sprite _checkmark;
+
+
+
+        ///  PRIVATE VARIABLES         ///
+
+        ///  PRIVATE METHODS           ///
+
+        ///  LISTNER METHODS           ///
+
+        ///  PUBLIC API                ///
+        public void SetObjective(Objective obj)
+        { 
+            _name.text = obj.Name;
+            _description.text = obj.Description;
+        }
+
+        public void Completed()
+        { 
+            _checkSpot.sprite = _checkmark;
+        }
+        ///  IMPLEMENTATION            ///
+
+    }
+}
