dd681b3760ada75ab1f412bcc70807ada12a9afa
shoshanimayan <shoshanimayan@gmail.com>
2023-11-28 18:18:12 -0800

    pause fix
    -look at why sometimes start in play
    - improve npc camera view with custom cam views
    -draw
    -text entry
    -drag

--- Assets/Internal/Scripts/Player/CameraFocuserView.cs
+++ Assets/Internal/Scripts/Player/CameraFocuserView.cs
@@ -17,12 +17,14 @@ namespace Player
         private Transform _originalFollow;
         private Camera _camera;
         private Transform _focus=null;
+        private Vector3 _tempPos;
         ///  PRIVATE METHODS           ///
          private void Awake()
          {
             _camera=Camera.main;
            _virCam = GetComponent<CinemachineVirtualCamera>();
             _originalFollow = _virCam.Follow;
+            _tempPos= _tempFocus.position;
 
          }
         ///  PUBLIC API                ///
@@ -54,6 +56,8 @@ namespace Player
         public void UnfocusCamera()
         {
            _focus = null;
+            _tempFocus.position = _tempPos;
+
         }
 
 
